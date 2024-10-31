2d424f305826f718b808be7661959217791d5319
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-31 23:48:25 +0300

    Token not found added to validation method

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -44,11 +44,16 @@ def validate():
     """User validation endpoint"""
     token = request.headers.get('Authorization').split()[1]
     try:
-        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        return jsonify({'status': 'valid', 'user': data['username']})
+        if token:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            return jsonify({'status': 'valid', 'user': data['username']})
+        else:
+            logger.error("Token not found")
+            return jsonify({'status': 'Token not found'}), 401
     except jwt.ExpiredSignatureError:
         logger.error("Token expired")
         return jsonify({'status': 'expired'}), 401
     except jwt.InvalidTokenError:
         logger.error("Token invalid")
         return jsonify({'status': 'invalid'}), 401
+    except jwt.
