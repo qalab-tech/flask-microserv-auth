02a519ce5f70fe516425152505a9c67a0614f7b9
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-11-30 00:08:56 +0300

    Prometheus metrics added

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from app.controllers.auth_controller import auth_bp
+from prometheus_flask_exporter import PrometheusMetrics
 
 
 def create_app():
     app = Flask(__name__)
     app.url_map.strict_slashes = False
+    PrometheusMetrics(app)
     # Register BluePrint
     app.register_blueprint(auth_bp, url_prefix='/')
 
